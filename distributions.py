cae218870816a722205bfe7155fa535866e2325a
Alberto Bietti <alberto@bietti.me>
2014-06-11 13:00:23 +0200

    sufficient statistics for HSMM

--- distributions.py
+++ distributions.py
@@ -53,13 +53,16 @@ class Gaussian(Distribution):
     def sample(self, size=1):
         return np.random.multivariate_normal(self.mean, self.cov, size)
 
-    def new_sufficient_statistics(self, x, cluster_id, K):
-        return GaussianSufficientStatistics(x, cluster_id, K, self.dim)
+    def new_sufficient_statistics_hmm(self, x, cluster_id, K):
+        return GaussianSufficientStatisticsHMM(x, cluster_id, K, self.dim)
+
+    def new_sufficient_statistics_hsmm(self, x, cluster_id, K, D):
+        return GaussianSufficientStatisticsHSMM(x, cluster_id, K, D, self.dim)
 
     def online_max_likelihood(self, rho_obs, phi):
-        s0 = rho_obs.rho0.dot(phi)
-        self.mean = rho_obs.rho1.dot(phi) / s0
-        self.cov = rho_obs.rho2.dot(phi) / s0 - self.mean[:,nax] * self.mean
+        s0, s1, s2 = rho_obs.get_statistics(phi)
+        self.mean = s1 / s0
+        self.cov = s2 / s0 - self.mean[:,nax] * self.mean
 
 # for euclidian K-means or isotropic Gaussian
 class SquareDistance(Distribution):
@@ -135,11 +138,15 @@ class KL(Distribution):
     def pdf(self, X):
         return np.exp(X.dot(np.log(self.mean)))
 
-    def new_sufficient_statistics(self, x, cluster_id, K):
-        return KLSufficientStatistics(x, cluster_id, K, self.mean.shape[0])
+    def new_sufficient_statistics_hmm(self, x, cluster_id, K):
+        return KLSufficientStatisticsHMM(x, cluster_id, K, self.dim)
+
+    def new_sufficient_statistics_hsmm(self, x, cluster_id, K, D):
+        return KLSufficientStatisticsHSMM(x, cluster_id, K, D, self.dim)
 
     def online_max_likelihood(self, rho_obs, phi):
-        self.mean = rho_obs.rho.dot(phi) / rho_obs.rho0.dot(phi)
+        s0, s1 = rho_obs.get_statistics(phi)
+        self.mean = s1 / s0
 
 class DurationDistribution(Distribution):
     def __init__(self, D):
@@ -150,6 +157,11 @@ class DurationDistribution(Distribution):
 
     def log_vec(self):
         return self.log_pmf(np.arange(1,self.D+1))
+
+    def d_frac(self):
+        v = self.log_vec()
+        D = np.hstack((np.cumsum(v[::-1])[::-1], 0.))
+        return D[1:] / D[:-1]
 
 class PoissonDuration(DurationDistribution):
     def __init__(self, lmbda, D):
@@ -175,20 +187,24 @@ class NegativeBinomial(DurationDistribution):
         return stats.nbinom.rvs(self.r, self.p, size=size)
 
 # Sufficient Statistics classes
-
 class SufficientStatistics(object):
+    def online_update(self, x, r, step):
+        raise NotImplementedError
+
+    def get_statistics(self, phi):
+        raise NotImplementedError
+
+class SufficientStatisticsHMM(SufficientStatistics):
     def __init__(self, cluster_id, K):
         self.cluster_id = cluster_id
         self.K = K
 
-    def online_update(self, x, r, step):
-        pass
-
-class GaussianSufficientStatistics(SufficientStatistics):
+class GaussianSufficientStatisticsHMM(SufficientStatisticsHMM):
     def __init__(self, x, cluster_id, K, size):
-        super(GaussianSufficientStatistics, self).__init__(cluster_id, K)
+        super(GaussianSufficientStatisticsHMM, self).__init__(cluster_id, K)
         # 1{Z_t = i}
         self.rho0 = np.zeros(self.K)
+        self.rho0[self.cluster_id] = 1.
         # 1{Z_t = i} x_t
         self.rho1 = np.zeros((size, self.K))
         self.rho1[:,self.cluster_id] = x
@@ -204,11 +220,15 @@ class GaussianSufficientStatistics(SufficientStatistics):
         self.rho2 = (1 - step) * self.rho2.dot(r)
         self.rho2[:,:,self.cluster_id] += step * x[:,nax] * x
 
-class KLSufficientStatistics(SufficientStatistics):
+    def get_statistics(self, phi):
+        return self.rho0.dot(phi), self.rho1.dot(phi), self.rho2.dot(phi)
+
+class KLSufficientStatisticsHMM(SufficientStatisticsHMM):
     def __init__(self, x, cluster_id, K, size):
-        super(KLSufficientStatistics, self).__init__(cluster_id, K)
+        super(KLSufficientStatisticsHMM, self).__init__(cluster_id, K)
         # 1{Z_t = i}
         self.rho0 = np.zeros(self.K)
+        self.rho0[self.cluster_id] = 1.
         # 1{Z_t = i} x_t
         self.rho = np.zeros((size, self.K))
         self.rho[:,self.cluster_id] = x
@@ -216,5 +236,76 @@ class KLSufficientStatistics(SufficientStatistics):
     def online_update(self, x, r, step):
         self.rho0 = (1 - step) * self.rho0.dot(r)
         self.rho0[self.cluster_id] += step
-        self.rho = (1 - step) * self.rho.dot(r)
-        self.rho[:,self.cluster_id] += step * x
+        self.rho1 = (1 - step) * self.rho.dot(r)
+        self.rho1[:,self.cluster_id] += step * x
+
+    def get_statistics(self, phi):
+        return self.rho0.dot(phi), self.rho1.dot(phi)
+
+class SufficientStatisticsHSMM(SufficientStatistics):
+    def __init__(self, cluster_id, K, D):
+        self.cluster_id = cluster_id
+        self.K = K
+        self.D = D
+
+class GaussianSufficientStatisticsHSMM(SufficientStatisticsHSMM):
+    def __init__(self, x, cluster_id, K, D, size):
+        super(GaussianSufficientStatisticsHSMM, self).__init__(cluster_id, K, D)
+        # 1{Z_t = i}
+        self.rho0 = np.zeros((self.K, self.D))
+        self.rho0[self.cluster_id] = 1.
+        # 1{Z_t = i} x_t
+        self.rho1 = np.zeros((size, self.K, self.D))
+        self.rho1[:,self.cluster_id] = x[:,nax]
+        # 1{Z_t = i} x_t x_t'
+        self.rho2 = np.zeros((size, size, self.K, self.D))
+        self.rho2[:,:,self.cluster_id] = (x[:,nax] * x)[:,:,nax]
+
+    def online_update(self, x, r, step):
+        rho0 = np.zeros(self.rho0.shape)
+        rho0[:,0] = (1 - step) * np.tensordot(self.rho0, r)
+        rho0[self.cluster_id,0] += step
+        rho0[:,1:] = (1 - step) * self.rho0[:,:-1]
+        self.rho0 = rho0
+
+        rho1 = np.zeros(self.rho1.shape)
+        rho1[:,:,0] = (1 - step) * np.tensordot(self.rho1, r)
+        rho1[:,self.cluster_id,0] += step * x[:,nax]
+        rho1[:,:,1:] = (1 - step) * self.rho1[:,:,:-1]
+        self.rho1 = rho1
+
+        rho2 = np.zeros(self.rho2.shape)
+        rho2[:,:,:,0] = (1 - step) * np.tensordot(self.rho2, r)
+        rho2[:,:,self.cluster_id,0] += step * (x[:,nax] * x)[:,nax]
+        rho2[:,:,:,1:] = (1 - step) * self.rho2[:,:,:,:-1]
+        self.rho2 = rho2
+
+    def get_statistics(self, phi):
+        return np.tensordot(self.rho0, phi), np.tensordot(self.rho1, phi),
+            np.tensordot(self.rho2, phi)
+
+class KLSufficientStatisticsHSMM(SufficientStatisticsHSMM):
+    def __init__(self, x, cluster_id, K, size):
+        super(KLSufficientStatisticsHSMM, self).__init__(cluster_id, K)
+        # 1{Z_t = i}
+        self.rho0 = np.zeros((self.K, self.D))
+        self.rho0[self.cluster_id] = 1.
+        # 1{Z_t = i} x_t
+        self.rho1 = np.zeros((size, self.K, self.D))
+        self.rho1[:,self.cluster_id] = x[:,nax]
+
+    def online_update(self, x, r, step):
+        rho0 = np.zeros(self.rho0.shape)
+        rho0[:,0] = (1 - step) * np.tensordot(self.rho0, r)
+        rho0[self.cluster_id,0] += step
+        rho0[:,1:] = (1 - step) * self.rho0[:,:-1]
+        self.rho0 = rho0
+
+        rho1 = np.zeros(self.rho1.shape)
+        rho1[:,:,0] = (1 - step) * np.tensordot(self.rho1, r)
+        rho1[:,self.cluster_id,0] += step * x[:,nax]
+        rho1[:,:,1:] = (1 - step) * self.rho1[:,:,:-1]
+        self.rho1 = rho1
+
+    def get_statistics(self, phi):
+        return np.tensordot(self.rho0, phi), np.tensordot(self.rho1, phi)
